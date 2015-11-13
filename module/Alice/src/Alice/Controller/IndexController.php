1565dd1dff7066c041b80a4ecf1402b54ec6bd10
vietduu <hungvietduu@gmail.com>
2015-11-13 19:10:25 +0700

    clone code base to module "alice"

--- module/Alice/src/Alice/Controller/IndexController.php
+++ module/Alice/src/Alice/Controller/IndexController.php
@@ -1,21 +1,69 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace Alice\Controller;
-
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Bob\Helper\ServiceConfigHelper;
 
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
-    {
-        return new ViewModel();
-    }
+	public function indexAction()
+	{
+		$view = new ViewModel(array(
+			'all_products' => $this->fetchAllProductTypes(),
+			'product_type' => $this->getProductType(1),
+			'all_general_products' => $this->fetchAllGeneralProducts(),
+			'products_by_type_id' => $this->getProductsByProductTypeId(1),
+			));
+		return $view;
+	}
+
+	public function registerAction()
+	{
+		$view = new ViewModel();
+		$view->setTemplate('bob/pet/index');
+		return $view;
+	}
+
+	public function getProductType($id)
+	{
+		$productTypeMapper = $this->getProductTypeServiceConfig();
+		
+		$_id = (int) $id;
+		return $productTypeMapper->getById($_id);
+	}
+
+
+	private function getProductTypeServiceConfig(){
+		return ServiceConfigHelper::getServiceConfig($this,
+			'Bob\Model\DataObject\ProductType',
+			'product_type',
+			'Bob\Model\DataMapper\ProductTypeMapper');
+	}
+
+	public function fetchAllProductTypes()
+	{
+		$products = $this->getProductTypeServiceConfig();
+		return $products->fetchAll();
+	}
+
+	private function getGeneralProductServiceConfig()
+	{
+		return ServiceConfigHelper::getServiceConfig($this,
+			'Bob\Model\DataObject\GeneralProduct',
+			'general_product',
+			'Bob\Model\DataMapper\GeneralProductMapper'
+			);
+	}
+
+	public function fetchAllGeneralProducts()
+	{
+		$general_products = $this->getGeneralProductServiceConfig();
+		return $general_products->fetchAll();
+	}
+
+	public function getProductsByProductTypeId($id)
+	{
+		$products = $this->getGeneralProductServiceConfig();
+		return $products->getProductsByProductTypeId($id);
+	}
 }
